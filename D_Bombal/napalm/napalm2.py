c4c0bacfb894c48d7ee15a86a8ef61eb00c852d6
Vancil_Allen <Vancil_Allen@asdk12.org>
2025-05-22 16:12:13 -0800

    more napalm

---
+++ D_Bombal/napalm/napalm2.py
@@ -0,0 +1,18 @@
+from napalm import get_network_driver
+import json
+
+driver = get_network_driver('ios')
+iosvl2 = driver('192.168.122.72', 'david', 'cisco')
+iosvl2.open()
+
+ios_output = iosvl2.get_mac_address_table()
+print(json.dumps(ios_output, indent=4))
+
+ios_output = iosvl2.get_interfaces()
+print(json.dumps(iosvl2, sort_keys=True, indent=4))
+
+ios_output = iosvl2.get_interfaces_counters()
+print(json.dumps(iosvl2, sort_keys=True, indent=4))
+
+ios_output = iosvl2.get_facts()
+print(ios_output)
